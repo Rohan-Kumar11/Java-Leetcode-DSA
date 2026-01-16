032eb39198d02ede65cded7e8eb6593f54f6fc79
Rohan-Kumar11 <rohan971161@gmail.com>
2026-01-16 14:31:02 +0530

    Add sum_Num.java to calculate the sum of two numbers

---
+++ sum_Num.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sum_Num {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1= input.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2= input.nextInt();
+        int result=num1+num2;
+        System.out.print("the sum of number is :"+ result);
+    }
+}
