569eb1be7e628bf5c375b57ad23051b9f4acf38e
Rohan-Kumar11 <rohan971161@gmail.com>
2026-01-23 23:31:48 +0530

    Add Concatenation_of_Array.java with main logic
    Implement a Java program to concatenate an array.

---
+++ src/Array/Concatenation_of_Array.java
@@ -0,0 +1,49 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Concatenation_of_Array {
+//    Leet Code: 1929. Concatenation of Array
+    /*
+    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+     */
+
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = input.nextInt();
+
+        int[] nums = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i< size; i++){
+            nums[i] = input.nextInt();
+        }
+        int[] ans = getConcatenation(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static public int[] getConcatenation(int[] nums) {
+        int[] ans= new int[2*nums.length];
+        int n= nums.length;
+        for(int i=0 ; i<nums.length ; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
